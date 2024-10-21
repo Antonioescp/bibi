86b5eb6f2ffeb5b78aac3db76ee0bb7b71a76fb7
Antonio Escorcia <packageinstaller2@gmail.com>
2024-10-21 09:21:01 -0600

    Doc: Documentación de Exception.hpp

--- src/Core/Exceptions/Exception.hpp
+++ src/Core/Exceptions/Exception.hpp
@@ -12,8 +12,20 @@
 
 namespace Bibi::Core::Exceptions {
 
+    /**
+     * Clase para las excepciones de la aplicación.
+     * Contiene la traza de la pila y la ubicación del error.
+     * @note esta clase debe ser heredada para crear excepciones específicas.
+     */
     class Exception : public std::exception {
     public:
+
+        /**
+         * Crea una instancia de Exception.
+         * @param message Un mensaje explicativo del error.
+         * @param stacktrace La pila de llamadas al momento de la excepción.
+         * @param location La ubicación del error.
+         */
         explicit Exception(
                 const char *message,
                 std::stacktrace stacktrace = std::stacktrace::current(),
@@ -21,11 +33,23 @@ namespace Bibi::Core::Exceptions {
                 : std::exception(message), _stacktrace(std::move(stacktrace)), _location(location) {
         }
 
+        /**
+         * Devuelve la traza de la pila.
+         * @return Traza de la pila de llamadas.
+         */
         [[nodiscard]] const std::stacktrace &stacktrace() const;
+
+        /**
+         * Devuelve la ubicación del error.
+         * @return Ubicación del error en el código fuente.
+         */
         [[nodiscard]] const std::source_location &location() const;
 
     private:
+        // Pila de llamadas
         std::stacktrace _stacktrace;
+
+        // Ubicación del error en código fuente
         std::source_location _location;
     };
 
