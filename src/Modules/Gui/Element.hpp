64459800df97ece8fda8da6e2730b106ddf3ab9f
Antonio Escorcia <packageinstaller2@gmail.com>
2024-10-21 09:51:30 -0600

    Doc: Para Element.hpp y IElement.hpp

--- src/Modules/Gui/Element.hpp
+++ src/Modules/Gui/Element.hpp
@@ -18,22 +18,69 @@ namespace Bibi::Modules::Gui {
     public:
         ~Element() override = default;
 
+        /**
+         * Crea una instancia de Element.
+         * @param application La aplicación a la que pertenece el elemento.
+         */
         explicit Element(Core::Application *application);
 
+        /**
+         * Crea una instancia de Element.
+         */
         Element() = default;
 
+        /**
+         * Se realizan todas las configuraciones necesarias para el elemento.
+         * @note IMPORTANTE: Llamar a la implementación base de este método Element::setUp() en las clases
+         * derivadas para asegurar que los elementos hijos sean configurados adecuadamente.
+         * @note Aquí se pueden agregar elementos hijos, configurar el elemento, etc.
+         * @note Se recomienda llamar Element::setUp() al final de la implementación de este método en las clases derivadas.
+         */
         void setUp() override;
 
+        /**
+         * Se actualiza el elemento, utilice este método para renderizar el elemento.
+         * @note IMPORTANTE: Llamar a la implementación base de este método Element::update() en las clases
+         * derivadas para asegurar que los elementos hijos sean actualizados adecuadamente. Este método
+         * se puede llamar donde considere adecuado para renderizar los elementos hijos.
+         */
         void update() override;
 
+        /**
+         * Se realiza la limpieza de los recursos utilizados por el elemento.
+         * @note IMPORTANTE: Llamar a la implementación base de este método Element::tearDown() en las clases
+         * derivadas para asegurar que los elementos hijos sean limpiados adecuadamente.
+         * @note Se recomienda llamar Element::tearDown() al inicio de la implementación de este método en las clases derivadas.
+         */
         void tearDown() override;
 
+        /**
+         * Agrega un elemento hijo a este elemento.
+         * @param element El elemento hijo a agregar.
+         * @note El elemento hijo será adecuadamente administrado por el elemento padre.
+         * @note El elemento se agrega de manera diferida, es decir, se agrega al inicio del siguiente ciclo de actualización.
+         */
         void addElement(std::unique_ptr<IElement> element) override;
 
+        /**
+         * Elimina un elemento hijo de este elemento.
+         * @param element El elemento hijo a eliminar.
+         * @note El elemento se elimina de manera diferida, es decir, se elimina al inicio del siguiente ciclo de actualización.
+         */
         void removeElement(IElement *element) override;
 
+        /**
+         * Elimina todos los elementos hijos de este elemento que tengan una etiqueta específica.
+         * @param tag La etiqueta de los elementos a eliminar.
+         * @note Los elementos se eliminan de manera diferida, es decir, se eliminan al inicio del siguiente ciclo de actualización.
+         */
         void removeElements(std::string tag) override;
 
+        /**
+         * Elimina todos los elementos hijos de este elemento que sean del tipo especificado.
+         * @tparam TElement El tipo de elemento a eliminar.
+         * @note Los elementos se eliminan de manera diferida, es decir, se eliminan al inicio del siguiente ciclo de actualización.
+         */
         template <typename TElement>
         requires std::derived_from<TElement, IElement>
         void removeElements() {
@@ -63,13 +110,40 @@ namespace Bibi::Modules::Gui {
         IElement *getChildByTag(std::string_view tag) override;
 
     protected:
+        /**
+         * Aplicacion a la que pertenece el elemento.
+         */
         Core::Application *_application{nullptr};
+
+        /**
+         * Coleccion de todos los elementos hijos de este elemento.
+         */
         std::vector<std::unique_ptr<IElement>> _childrenElements{};
+
+        /**
+         * Coleccion de elementos hijos que se van a agregar en el siguiente ciclo de actualizacion.
+         */
         std::vector<std::unique_ptr<IElement>> _elementsToAdd{};
+
+        /**
+         * Coleccion de elementos hijos que se van a eliminar en el siguiente ciclo de actualizacion.
+         */
         std::vector<IElement*> _elementsToRemove{};
+
+        /**
+         * Etiqueta del elemento.=
+         */
         std::string _tag{};
+
+        /**
+         * Padre de este elemento.
+         */
         IElement *_parent{nullptr};
 
+        /**
+         * Maneja los elementos pendientes de agregar y eliminar.
+         * @note se llama en Element::setUp() y antes de iniciar la actualizacion de los elementos en Element::update().
+         */
         void handlePendingElements();
     };
 
--- src/Modules/Gui/IElement.hpp
+++ src/Modules/Gui/IElement.hpp
@@ -10,25 +10,93 @@
 #include <vector>
 
 #define GLFW_INCLUDE_NONE
+
 #include <GLFW/glfw3.h>
 #include "Core/Application.hpp"
 
 namespace Bibi::Modules::Gui {
-class IElement : public Core::Lifecycle::ILifecycleAware {
+
+    /**
+     * Interfaz para todos los elementos de la interfaz de usuario.
+     * @note Existe una implementacion base de esta clase llamada Element, la clase Element
+     * debe ser utilizada como base para todos los elementos de la interfaz de usuario.
+     */
+    class IElement : public Core::Lifecycle::ILifecycleAware {
     public:
         ~IElement() override = default;
+
+        /**
+         * Agrega un elemento hijo a este elemento.
+         * @param element El elemento hijo a agregar.
+         * @note El elemento hijo será adecuadamente administrado por el elemento padre.
+         */
         virtual void addElement(std::unique_ptr<IElement> element) = 0;
-        virtual void removeElement(IElement* element) = 0;
+
+        /**
+         * Elimina un elemento hijo de este elemento.
+         * @param element El elemento hijo a eliminar.
+         */
+        virtual void removeElement(IElement *element) = 0;
+
+        /**
+         * Elimina todos los elementos hijos de este elemento que tengan una etiqueta específica.
+         * @param tag La etiqueta de los elementos a eliminar.
+         */
         virtual void removeElements(std::string tag) = 0;
+
+        /**
+         * Elimina todos los elementos hijos de este elemento.
+         */
         virtual void clearElements() = 0;
-        virtual std::vector<std::unique_ptr<IElement>>& getElements() = 0;
+
+        /**
+         * Obtiene todos los elementos hijos de este elemento.
+         * @return Todos los elementos hijos de este elemento.
+         */
+        virtual std::vector<std::unique_ptr<IElement>> &getElements() = 0;
+
+        /**
+         * Establece la etiqueta de este elemento.
+         * @param tag La etiqueta de este elemento.
+         */
         virtual void setTag(std::string tag) = 0;
+
+        /**
+         * Obtiene la etiqueta de este elemento.
+         * @return La etiqueta de este elemento.
+         */
         virtual std::string getTag() = 0;
-        virtual void setParent(IElement* parent) = 0;
-        virtual IElement* getParent() = 0;
-        virtual Core::Application * getApplication() = 0;
+
+        /**
+         * Establece el padre de este elemento.
+         * @param parent El padre de este elemento.
+         */
+        virtual void setParent(IElement *parent) = 0;
+
+        /**
+         * Obtiene el padre de este elemento.
+         * @return El padre de este elemento.
+         */
+        virtual IElement *getParent() = 0;
+
+        /**
+         * Obtiene la aplicación a la que pertenece este elemento.
+         * @return La aplicación a la que pertenece este elemento.
+         */
+        virtual Core::Application *getApplication() = 0;
+
+        /**
+         * Establece la aplicación a la que pertenece este elemento.
+         * @param application La aplicación a la que pertenece este elemento.
+         */
         virtual void setApplication(Core::Application *application) = 0;
-        virtual IElement* getChildByTag(std::string_view tag) = 0;
+
+        /**
+         * Obtiene un elemento hijo de este elemento por su etiqueta.
+         * @param tag La etiqueta del elemento hijo a obtener.
+         * @return El elemento hijo con la etiqueta especificada o nullptr si no se encuentra.
+         */
+        virtual IElement *getChildByTag(std::string_view tag) = 0;
     };
 } // Modules
 
